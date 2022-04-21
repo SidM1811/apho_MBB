83f778c9f71908f338cb145cae7458bc8c8d08ed
cykadolkar <cykadolkar@gmail.com>
2022-04-21 20:35:56 +0530

    Mobile color gradient removed as per Praveen's request.

--- Mobile.js
+++ Mobile.js
@@ -41,12 +41,12 @@ class Mobile {
     render() {
         context.strokeStyle = "#000000";
         context.fillStyle = "rgba(128,128,128,0.5)";
-        var my_gradient = context.createLinearGradient(this.points[0].x, this.points[0].y, this.points[1].x, this.points[1].y);
+ /*       var my_gradient = context.createLinearGradient(this.points[0].x, this.points[0].y, this.points[1].x, this.points[1].y);
         my_gradient.addColorStop(0, "black");
         my_gradient.addColorStop(0.03, "rgba(128,128,128,0.5)");
         my_gradient.addColorStop(0.97, "rgba(128,128,128,0.5)");
         my_gradient.addColorStop(1, "black");
-        context.fillStyle = my_gradient;
+        context.fillStyle = my_gradient;*/
         context.beginPath();
         context.moveTo(this.points[3].x, this.points[3].y);
         for(let point of this.points) {
